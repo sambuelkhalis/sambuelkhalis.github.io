0cdfcaf25efdaf1c3109cd51d96b5cb4040935de
sambuelkhalis <sambuelkhalis@gmail.com>
2022-03-23 22:08:10 +0700

    Add Project Section ( Alpha Version )

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY > 430) {
+        if(this.scrollY > 500) {
             $('.navbar').addClass("sticky")
         } else {
             $('.navbar').removeClass("sticky")
         }
+
+        if(this.scrollY > 600) {
+            $('.button_up').addClass("show")
+        } else {
+            $('.button_up').removeClass("show")
+        }
     })
+});
+
+// Ease when button_up click go to up Script
+$('.button_up').click(function() {
+    $('html').animate({scrollTop: 0});
+});
+
+// Typing Animation Script
+var typed = new Typed(".typing", {
+    strings: ["Freelancer", "Web Developer", "Game Developer", "Photographer", "Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing_2", {
+    strings: ["Freelancer", "Web Developer", "Game Developer", "Photographer", "Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
 });
