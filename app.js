92b21ad93bb46b1751e5b9f97545a64a2984667f
sambuelkhalis <sambuelkhalis@gmail.com>
2022-03-24 16:57:26 +0700

    Release Full Version with Responsive ( Beta )

--- app.js
+++ app.js
@@ -1,35 +1,50 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 500) {
-            $('.navbar').addClass("sticky")
-        } else {
-            $('.navbar').removeClass("sticky")
-        }
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 500) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
 
-        if(this.scrollY > 600) {
-            $('.button_up').addClass("show")
-        } else {
-            $('.button_up').removeClass("show")
-        }
-    })
+    if (this.scrollY > 600) {
+      $(".button_up").addClass("show");
+    } else {
+      $(".button_up").removeClass("show");
+    }
+  });
 });
 
 // Ease when button_up click go to up Script
-$('.button_up').click(function() {
-    $('html').animate({scrollTop: 0});
+$(".button_up").click(function () {
+  $("html").animate({ scrollTop: 0 });
 });
 
-// Typing Animation Script
-var typed = new Typed(".typing", {
+const mediaQuery1 = window.matchMedia("(min-width: 1024px)");
+
+if (mediaQuery1.matches) {
+  // Typing Animation Script
+  var typed = new Typed(".typing", {
     strings: ["Freelancer", "Web Developer", "Game Developer", "Photographer", "Youtuber"],
     typeSpeed: 110,
     backSpeed: 60,
-    loop: true
-});
+    loop: true,
+  });
+}
 
 var typed = new Typed(".typing_2", {
-    strings: ["Freelancer", "Web Developer", "Game Developer", "Photographer", "Youtuber"],
+  strings: ["Freelancer", "Web Developer", "Game Developer", "Photographer", "Youtuber"],
+  typeSpeed: 110,
+  backSpeed: 60,
+  loop: true,
+});
+
+const mediaQuery3 = window.matchMedia("(max-width: 480px)");
+
+if (mediaQuery3.matches) {
+  var typed = new Typed(".typing", {
+    strings: ["Freelancer", "Web Develop", "Game Develop", "Photographer", "Youtuber"],
     typeSpeed: 110,
     backSpeed: 60,
-    loop: true
-});
+    loop: true,
+  });
+}
